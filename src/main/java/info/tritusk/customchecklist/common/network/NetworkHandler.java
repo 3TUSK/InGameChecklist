a52aa732531b5372dbdf521818498888669116ea
3TUSK <urey.s.knowledge@gmail.com>
2016-04-10 21:01:25 -0400

    What...
    Network thing is annoy.
    
    Signed-off-by: 3TUSK <urey.s.knowledge@gmail.com>

--- src/main/java/info/tritusk/customchecklist/common/network/NetworkHandler.java
+++ src/main/java/info/tritusk/customchecklist/common/network/NetworkHandler.java
@@ -2,9 +2,13 @@ package info.tritusk.customchecklist.common.network;
 
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class NetworkHandler {
 	
 	public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel("CustomChecklist");
 
+	public static void init() {
+		INSTANCE.registerMessage(PacketSyncChecklist.class, PacketSyncChecklist.class, 0, Side.CLIENT);
+	}
 }
