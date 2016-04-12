3f84e8eb51b87ff146133fe63f1a98c4b966fb84
3TUSK <urey.s.knowledge@gmail.com>
2016-04-11 22:11:24 -0400

    New command for manual update
    Signed-off-by: 3TUSK <urey.s.knowledge@gmail.com>

---
+++ src/main/java/info/tritusk/customchecklist/common/command/CommandRemoteTasksSync.java
@@ -0,0 +1,27 @@
+package info.tritusk.customchecklist.common.command;
+
+import info.tritusk.customchecklist.common.network.NetworkHandler;
+import info.tritusk.customchecklist.common.network.PacketSyncChecklist;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+
+public class CommandRemoteTasksSync extends CommandBase {
+
+	@Override
+	public String getCommandName() {
+		return "customchecklistsync";
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender sender) {
+		return "/customchecklistsync - Keep your checklist synchronize with remote server";
+	}
+
+	@Override
+	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+		//Blame @bakaxyf, the following code WILL CRASH GAME, and it will be updated soon
+		NetworkHandler.INSTANCE.sendToAll(new PacketSyncChecklist(args[0], args[1]));
+	}
+
+}
