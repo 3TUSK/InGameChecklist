498447e7c54b4b48ee95e37bad12d26d74d11cc7
3TUSK <urey.s.knowledge@gmail.com>
2016-11-06 10:07:03 -0500

    Screw up you task manager

--- src/main/java/info/tritusk/ingamechecklist/common/task/TaskEntryManager.java
+++ src/main/java/info/tritusk/ingamechecklist/common/task/TaskEntryManager.java
@@ -7,10 +7,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -110,43 +108,13 @@ public class TaskEntryManager implements ITaskManager {
 	public boolean saveTo(final File file) {
 		try {
 			Element list = xmlFile.getDocumentElement();
-			if (list == null || !list.getAttribute("name").equals("checklist")) {
-				Element main = xmlFile.createElement("checklist"); // This also automatically appends element
-				tasks.entrySet().forEach(task -> {
-					Element aTask = xmlFile.createElement("task");
-					aTask.setAttribute("name", task.getKey());
-					aTask.setTextContent(task.getValue().description());
-					main.appendChild(aTask);
-				});
-			} else {
-				List<String> knownTasks = new ArrayList<>(tasks.keySet());
-				List<String> reduntant = new ArrayList<>();
-				NodeList taskNodes = list.getElementsByTagName("task");
-				final int length = taskNodes.getLength();
-				for (int i = 0; i < length; i++) {
-					Element e = (Element)taskNodes.item(i);
-					if (e != null) {
-						if (knownTasks.contains(e.getAttribute("name"))) {
-							e.setTextContent(tasks.get(e.getAttribute("name")).description());
-							reduntant.add(e.getAttribute("name"));
-						} else {
-							list.removeChild(e);
-						} 
-					} else {
-						IClProxy.log.trace("Found a null element");
-					}
-				}
-				
-				knownTasks.removeAll(reduntant);
-				reduntant.clear();
-				
-				knownTasks.forEach(s -> {
-					Element newTask = xmlFile.createElement("task");
-					newTask.setAttribute("name", s);
-					newTask.setTextContent(tasks.get(s).description());
-					list.appendChild(newTask);
-				});
-			}			
+			Element main = xmlFile.createElement("checklist");
+			tasks.values().forEach(task -> append(xmlFile, task, main));
+			if (list == null) {
+				xmlFile.appendChild(main);
+			} else { // Replace will overwrite the previous data, which is actually a work around for now.
+				xmlFile.replaceChild(main, list);
+			}
 			writer.transform(new DOMSource(xmlFile), new StreamResult(new FileOutputStream(file, false)));
 			IClProxy.log.info("Successfully saved local checklist.");
 			tasks.clear();
@@ -158,6 +126,13 @@ public class TaskEntryManager implements ITaskManager {
 				e.printStackTrace();
 			return false;
 		}
+	}
+	
+	private void append(Document doc, ITask task, Element element) {
+		Element toAppend = doc.createElement("task");
+		toAppend.setAttribute("name", task.name());
+		toAppend.setTextContent(task.description());
+		element.appendChild(toAppend);
 	}
 	
 	public Collection<ITask> getTasks() {
