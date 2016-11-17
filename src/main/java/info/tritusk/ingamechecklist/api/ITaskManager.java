9878a84244816025ac098c0150d4666f68afdf73
3TUSK <urey.s.knowledge@gmail.com>
2016-11-16 22:11:54 -0500

    Add javadoc, remove some exp stuff

--- src/main/java/info/tritusk/ingamechecklist/api/ITaskManager.java
+++ src/main/java/info/tritusk/ingamechecklist/api/ITaskManager.java
@@ -5,23 +5,66 @@ import java.io.InputStream;
 import java.util.Collection;
 
 import javax.annotation.Nullable;
-
+/**
+ * A <code>TaskManager</code> is an object that capable to (de)serialize 
+ * tasks from/into {@link InputStream stream}/{@link File file}, as well 
+ * as add/get/modify {@link ITask tasks}.
+ * @author 3TUSK
+ * @since 0.1.0
+ */
 public interface ITaskManager {
 	
+	/**
+	 * Call for initialize the manager instance.
+	 * @return True if initialization is successful, false for otherwise.
+	 */
 	boolean init();
 	
+	/**
+	 * Convenient method for availability checking. 
+	 * @return True if the manager is currently available to handle {@link ITask tasks}, false for otherwise.
+	 */
 	boolean isInitialized();
 	
+	/**
+	 * Deserialize tasks from given input stream.
+	 * @param input Serialized tasks, may not be null
+	 * @return True if load tasks successfully, false for otherwise
+	 */
 	boolean loadFrom(final InputStream input);
 	
+	/**
+	 * Serialize all tasks into given {@link File file} instance.
+	 * @param file Destination of tasks.
+	 * @return True if tasks are successfully saved, false for otherwise
+	 */
 	boolean saveTo(final File file);
 	
+	/**
+	 * A convenient method for iterating all tasks.
+	 * @return An immutable collection that contains all {@link ITask tasks}.
+	 */
 	Collection<ITask> getTasks();
 	
+	/**
+	 * Add a {@link ITask tasks} instance to the manager instance.
+	 * @param task Task to add
+	 * @return True if the process is successful
+	 */
 	boolean addTask(ITask task);
 	
+	/**
+	 * Remove a {@link ITask tasks} instance from the manager instance.
+	 * @param task Task to remove
+	 * @return True if the process is successful
+	 */
 	boolean removeTask(ITask task);
 	
+	/**
+	 * Get a task from this manager based on given name.
+	 * @param name The name of request task
+	 * @return A {@link ITask task} with its {@link ITask#name() name} of given name, or null if such task does not exist
+	 */
 	@Nullable
 	ITask getByName(String name);
 
--- src/main/java/info/tritusk/ingamechecklist/common/IClConfig.java
+++ src/main/java/info/tritusk/ingamechecklist/common/IClConfig.java
@@ -1,21 +1,9 @@
 package info.tritusk.ingamechecklist.common;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-
 import net.minecraftforge.common.config.Config;
 
 @Config(modid = "ingame_checklist", name = "InGameChecklist", type = Config.Type.INSTANCE)
 public final class IClConfig {
-	
-	public static enum MergePolicy {
-		MERGE, 
-		OVERRIDE, 
-		DISABLE
-	}
 	
 	@Config.Comment("Set to true to enable remote tasks retrieval")
 	@Config.LangKey("ingame_checklist.config.remoteTasksRetrive")
@@ -25,44 +13,8 @@ public final class IClConfig {
 	@Config.LangKey("ingame_checklist.config.remoteTasksURL")
 	public static String remoteTaskURL = "";
 		
-	@Config.Comment("Merging policy being applied during retrieval. Has no effect when remote retrieval is disabled")
-	@Config.LangKey("ingame_checklist.config.mergePolicy")
-	public static String mergePolicy = "DISABLE";
-		
 	@Config.Comment("Enable debug mode to see verbose output")
 	@Config.LangKey("ingame_checklist.config.debug")
 	public static boolean debug = false;
-	
-	private static MergePolicy policy;
-	
-	public static MergePolicy getMergePolicy() {
-		policy = MergePolicy.valueOf(mergePolicy);
-		if (policy == null)
-			policy = MergePolicy.DISABLE;
-		return policy;
-	}
-	
-	static InputStream remoteTasks;
-	
-	public static void initConfig() {
-		policy = MergePolicy.valueOf(mergePolicy);
-		if (policy == null)
-			policy = MergePolicy.DISABLE;
-		
-		if (remoteTaskURL != null && !remoteTaskURL.equals("")) {
-			new Thread(() -> {
-				try {
-					URLConnection connection = new URL(remoteTaskURL).openConnection();
-					connection.setReadTimeout(10000);
-					remoteTasks = connection.getInputStream();
-				} catch (MalformedURLException e) {
-					IClProxy.log.error("The input url is wrongly formatted!");
-				} catch (IOException e) {
-					IClProxy.log.error("Fail to load tasks file from remote server.");
-					e.printStackTrace();
-				}
-			}).start();
-		}
-	}
 
 }
